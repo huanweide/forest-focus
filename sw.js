93158d08fa0774dbcdc13e6158f42eb113221781
huanweide <3232553244@qq.com>
2026-06-03 20:00:47 +0800

    v3.5.2: Fix PWA install - proper icon sizes 192+512, manifest scope

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const V='forest-v6';
-const FILES=['./','./index.html','./manifest.json','./src/images/azusa/azusa_default.png','./src/images/azusa/azusa_box.png','./src/images/azusa/azusa_transparent.png'];
+const V='forest-v7';
+const FILES=['./','./index.html','./manifest.json','./src/images/azusa/azusa_default.png','./src/images/azusa/icon-192.png','./src/images/azusa/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(res&&res.status===200){let clone=res.clone();caches.open(V).then(c=>c.put(e.request,clone))}return res}).catch(()=>caches.match('./index.html'))))});
