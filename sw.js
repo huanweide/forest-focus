f551beff3303d689b11928808774786f054eb929
huanweide <3232553244@qq.com>
2026-06-03 16:49:36 +0800

    bump SW version to v4 for cache refresh

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
-const V='forest-v3';
+﻿const V='forest-v4';
 const FILES=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==V).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{if(res&&res.status===200){let clone=res.clone();caches.open(V).then(c=>c.put(e.request,clone))}return res}).catch(()=>caches.match('./index.html'))))});
+
